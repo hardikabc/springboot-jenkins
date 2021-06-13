361121f4c27394cae10b60d3aa1376272f301558
hardikabc <mukeshmca.singh@gmail.com>
2021-06-13 18:06:25 +0530

    code pushed

--- src/main/java/com/example/SpringbootJenkinsApplication.java
+++ src/main/java/com/example/SpringbootJenkinsApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootJenkinsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
 		SpringApplication.run(SpringbootJenkinsApplication.class, args);
+		System.out.println("End");
+		System.out.println("Hello Mukesh");
 	}
 
 }
